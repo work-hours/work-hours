9a48061050b26352fac67b5ec22f8e7fd868e9d1
msamgan <mohdsamgankhan@gmail.com>
2025-07-21 12:18:35 -0400

    removed `Courier_New` font reference and unused font preload links.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,9 +37,6 @@
         <link rel="icon" href="/logo.png" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/logo.png">
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
