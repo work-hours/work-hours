0d97e1669298b30b1deeac0fd2cda9ff17797590
msamgan <mohdsamgankhan@gmail.com>
2025-08-06 17:42:27 -0400

    refactor: add color property to Tag model and implement random color generation

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Carbon;
+use Override;
 
 /**
  * Class Tag
  *
  * @property int $id
  * @property string $name
+ * @property string $color
  * @property int $user_id
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
@@ -22,7 +25,16 @@ final class Tag extends Model
     protected $fillable = [
         'name',
         'user_id',
+        'color',
     ];
+
+    /**
+     * Generate a random hexadecimal color.
+     */
+    public static function generateRandomColor(): string
+    {
+        return '#' . mb_str_pad(dechex(mt_rand(0, 0xFFFFFF)), 6, '0', STR_PAD_LEFT);
+    }
 
     /**
      * Get the time logs associated with the tag.
@@ -43,8 +55,21 @@ final class Tag extends Model
     /**
      * Get the user that owns the tag.
      */
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The "booted" method of the model.
+     */
+    #[Override]
+    protected static function booted(): void
+    {
+        self::creating(function (Tag $tag): void {
+            if (! $tag->color) {
+                $tag->color = self::generateRandomColor();
+            }
+        });
     }
 }
