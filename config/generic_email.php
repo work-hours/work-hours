77f16c336d63d50681c53ff9ebdbae5e9dfed674
msamgan <mohdsamgankhan@gmail.com>
2025-09-16 10:52:09 -0400

    feat: add generic email domains list for validation

---
+++ config/generic_email.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'gmail.com',
+    'yahoo.com',
+    'hotmail.com',
+    'outlook.com',
+    'live.com',
+    'msn.com',
+    'aol.com',
+    'icloud.com',
+    'mail.com',
+    'gmx.com',
+    'yandex.com',
+    'inbox.com',
+    'rocketmail.com',
+    'lycos.com',
+    'rediffmail.com',
+];
